c3ca45dbb86ed6091abdb2cf2609a5c32f721791
Yulun Zhang <yulunzha@usc.edu>
2019-09-06 17:11:50 -0700

    add demo websocket implementation which sends data to the overlay of Ohmni

--- app.js
+++ app.js
@@ -1,6 +1,31 @@
-var express = require('express');
+var http = require('http');
+const express = require('express');
+const enableWs = require('express-ws')
+var overlayWs = null;
+// var ws = require('websocket'); 
+
+// set up http server
 var port = process.env.PORT || 3000;
-var app = express();
+const app = express();
+
+enableWs(app)
+
+app.ws('/echo', (ws, req) => {
+    ws.on('message', msg => {
+        console.log("message from client: " + msg);
+        overlayWs = ws;
+        const message = "Connection to overlay established"
+        console.log(message);
+        ws.send(message);
+    });
+
+    // called when the connection is closed
+    ws.on('close', () => {
+        console.log('Socket connection closed');
+    });
+})
+
+
 app.get('/', function (req, res) {
     res.send(JSON.stringify({ Hello: 'World'}));
 });
@@ -9,9 +34,16 @@ app.post('/android', function (req, res) {
     console.log(req);
     res.send(JSON.stringify(
         {
-            Android: "message gotten"
+            server: "message gotten"
         }));
+    try {
+        overlayWs.send("gotten message from Android and I am forwarding the message to you");
+    } catch (error) {
+        console.log("Send websocket message failed");
+    }
 });
+
+
 app.listen(port, function () {
     console.log(`Example app listening on port ${port}!`);
 });
